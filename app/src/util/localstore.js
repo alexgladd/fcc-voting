4da4f109125ca685c41f6c4fd2e3b97e97e476e3
Alex Gladd <alex@alexgladd.com>
2018-01-02 20:55:53 -0500

    Add local storage for persisting user vote record

--- app/src/util/localstore.js
+++ app/src/util/localstore.js
@@ -1,6 +1,7 @@
 // local storage for persisting user data
 
 const USER_INFO_KEY = 'mern_app_user_info';
+const VOTES_KEY = 'voting_app_user_votes';
 
 const setUserInfo = (userInfo) => {
   localStorage.setItem(USER_INFO_KEY, JSON.stringify(userInfo));
@@ -13,6 +14,18 @@ const clearUserInfo = () => {
 const getUserInfo = () => {
   try {
     return JSON.parse(localStorage.getItem(USER_INFO_KEY));
+  } catch(e) {
+    return null;
+  }
+}
+
+const setUserVotes = (votes) => {
+  localStorage.setItem(VOTES_KEY, JSON.stringify(votes));
+}
+
+const getUserVotes = () => {
+  try {
+    return JSON.parse(localStorage.getItem(VOTES_KEY));
   } catch(e) {
     return null;
   }
