495fb987d4aafc5ecaf79f052dfd18d47deaad6a
Alex Gladd <alex@alexgladd.com>
2018-01-10 20:59:26 -0500

    Add links to homepage heros

--- app/src/containers/Home.js
+++ app/src/containers/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { requestAllPolls } from '../actions/polls';
 import PollSummary from '../components/PollSummary';
 import './Home.css';
@@ -8,16 +9,17 @@ const Hero = () => (
   <div className="Hero">
     <h1>Welcome to Pollster!</h1>
     <p>Pollster is a quick and easy voting app. Create and share polls with
-    friends, family, and followers. Log in to get started creating new polls,
-    and check out the polls below for inspiration!</p>
+    friends, family, and followers. <Link to="/login">Log in</Link> to get
+    started creating new polls, and check out the polls below for inspiration!</p>
   </div>
 );
 
 const AuthHero = () => (
   <div className="Hero">
     <h1>Welcome back to Pollster!</h1>
-    <p>Use the buttons above to create a new poll or view your existing polls.
-    Check out polls from other users below!</p>
+    <p>Use the buttons above to create a <Link to="/poll/new">new</Link> poll
+    or <Link to="/profile">view</Link> your existing polls. Check out polls from
+    other users below!</p>
   </div>
 );
 
